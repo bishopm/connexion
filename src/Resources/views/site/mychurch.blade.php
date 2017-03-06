badbf480760e9ed3a23aec8a2b2ba95b5127b1b3
Michael Bishop <michael@umc.org.za>
2017-03-06 10:27:14 +0200

    Filterizr

--- src/Resources/views/site/mychurch.blade.php
+++ src/Resources/views/site/mychurch.blade.php
@@ -1,47 +1,37 @@
 @extends('connexion::templates.webpage')
 
 @section('css')
-<link rel="stylesheet" href="//cdn.datatables.net/1.10.12/css/jquery.dataTables.min.css">
+
 @stop
 
 @section('content')
 <img class="img-responsive" src="{{ asset('vendor/bishopm/images/webpageheader.png') }}">
 <div class="container">
-	<div class="row">
-	  <div class="col-md-9">
-	  	<h1>Meet the {{$setting['site_abbreviation']}} community</h1>
-	  	<table id="mychurch">
-	  		<thead>
-                <tr>
-                    <th></th>
-                    <th>Surname</th>
-                    <th>First name</th>
-                </tr>
-            </thead>
-            <tfoot>
-                <tr>
-                    <th></th>
-                    <th>Surname</th>
-                    <th>First name</th>
-                </tr>
-            </tfoot>
-	  	@foreach ($users as $user)
-	  		<tr><td><img width="100px" src="{{$user->individual->getMedia('image')->first()->getUrl()}}"></td><td>{{$user->individual->surname}}</td><td>{{$user->individual->firstname}}</td></tr>
-	  	@endforeach
-	  	</table>
-	  </div>
-	  <div class="col-md-3">
-	  <h3>Recent activity</h3>
-	  </div>
-	</div>
+  	<h1>Meet the {{$setting['site_abbreviation']}} community</h1>
+    <ul>
+       <input type="text" class="filtr-search" name="filtr-search" data-search>    
+       <li class="btn btn-primary" data-filter="all"> All </li>
+       <li class="btn btn-primary" data-filter="1"> Staff </li>
+    </ul>
+    <div class="row">
+        <div class="filtr-container">
+            @foreach ($users as $user)
+                <div class="col-xs-4 col-sm-6 col-md-12 filtr-item" data-category="1" data-sort="Busy streets">
+                    <img class="img-responsive" src="{{$user->individual->getMedia('image')->first()->getUrl()}}" alt="sample image">
+                    <span class="item-desc">{{$user->individual->firstname}} {{$user->individual->surname}}</span>
+                </div>
+            @endforeach
+        </div>
+    </div>	  	
 </div>
 @endsection
 
 @section('js')
-<script src="//cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+<script src="{{ asset('vendor/bishopm/js/jquery.filterizr.js') }}" type="text/javascript"></script>
 <script language="javascript">
-  $(document).ready(function() {
-    $('#mychurch').DataTable();
-  });
+    $(function() {
+        //Initialize filterizr with default options
+        $('.filtr-container').filterizr();
+    });
 </script>
 @stop
