fe383d629fbc789995a2fbdbd598ca5f3f2fb775
Michael Bishop <michael@umc.org.za>
2017-04-26 17:06:40 +0200

    People

--- src/Resources/views/site/subject.blade.php
+++ src/Resources/views/site/subject.blade.php
@@ -11,7 +11,7 @@
 			    <h4>Blog posts (News/Article)</h4>
 			    <ul class="list-unstyled">
 				    @foreach ($blogs as $blog)
-				    	<li>{{date("Y-m-d",strtotime($blog->created_at))}} <a href="{{url('/')}}/blog/{{$blog->slug}}">{{$blog->title}}</a> <a href="{{url('/')}}/users/{{$blog->individual->slug}}"><i>{{$blog->individual->firstname}} {{$blog->individual->surname}}</i></a></li>
+				    	<li>{{date("Y-m-d",strtotime($blog->created_at))}} <a href="{{url('/')}}/blog/{{$blog->slug}}">{{$blog->title}}</a> <a href="{{url('/')}}/people/{{$blog->individual->slug}}"><i>{{$blog->individual->firstname}} {{$blog->individual->surname}}</i></a></li>
 				    @endforeach
 				</ul>
 			</div>
@@ -21,7 +21,7 @@
 			    <h4>Sermons</h4>
 			    <ul class="list-unstyled">
 				    @foreach ($sermons as $sermon)
-				    	<li>{{date("Y-m-d",strtotime($blog->created_at))}} <a href="{{url('/')}}/sermons/{{$sermon->series->slug}}/{{$sermon->slug}}">{{$sermon->title}}</a> <a href="{{url('/')}}/users/{{$sermon->individual->slug}}"><i>{{$sermon->individual->firstname}} {{$sermon->individual->surname}}</i></a></li>
+				    	<li>{{date("Y-m-d",strtotime($blog->created_at))}} <a href="{{url('/')}}/sermons/{{$sermon->series->slug}}/{{$sermon->slug}}">{{$sermon->title}}</a> <a href="{{url('/')}}/people/{{$sermon->individual->slug}}"><i>{{$sermon->individual->firstname}} {{$sermon->individual->surname}}</i></a></li>
 				    @endforeach
 				</ul>
 			</div>
