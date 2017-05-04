90779a32d7ab2df77d6e967da0d2a76eaeef9f31
Michael Bishop <michael@umc.org.za>
2017-05-04 12:37:45 +0200

    Registration

--- src/Resources/views/emails/newuser.blade.php
+++ src/Resources/views/emails/newuser.blade.php
@@ -5,7 +5,7 @@ Thanks for signing up as a user on our site, {{$user->individual->firstname}}<br
 
 Click on the button below to confirm your email address and you'll be ready to go :) 
 
-Once you are logged in, take a moment to explore user menu in the top right hand corner of the site. Go and visit your user profile page, where you can upload a pic (and include a short bio if you like). The My {{$setting['site_abbreviation']}} link below gives you a look at the other users on the system. 
+Once you are logged in, take a moment to explore the user menu in the top right hand corner of the site. Go and visit your user profile page, where you can upload a pic (and include a short bio if you like). The My {{$setting['site_abbreviation']}} link below that introduces you to other church member who are users on the system. 
 
 You can also update your (and your family's) contact details through this menu.
 
@@ -14,5 +14,6 @@ Confirm my email address
 @endcomponent
 
 Enjoy exploring the site - let us know if you have any feedback or run into any trouble!<br>
+
 {{$setting['site_name']}}
 @endcomponent
