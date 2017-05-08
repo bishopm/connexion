8ca382de47d9dfc01046a702bf165ea1da16c79c
Michael Bishop <michael@umc.org.za>
2017-05-08 14:22:18 +0200

    Summernote for courses and books

--- src/Resources/views/books/create.blade.php
+++ src/Resources/views/books/create.blade.php
@@ -32,6 +32,7 @@
 
 @section('js')
     <script src="{{ asset('public/vendor/bishopm/summernote/summernote.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('public/vendor/bishopm/summernote/summernote-cleaner.js') }}" type="text/javascript"></script>
     <script src="{{ asset('public/vendor/bishopm/js/selectize.min.js') }}" type="text/javascript"></script>
     <script src="{{ asset('public/vendor/bishopm/js/croppie.js') }}" type="text/javascript"></script>
     <script type="text/javascript">
@@ -64,7 +65,18 @@
                 ['link', ['linkDialogShow', 'unlink']],
                 ['view', ['fullscreen', 'codeview']],
                 ['para', ['ul', 'ol', 'paragraph']]
-              ]
+              ],
+              cleaner:{
+                notTime: 2400, // Time to display Notifications.
+                action: 'paste', // both|button|paste 'button' only cleans via toolbar button, 'paste' only clean when pasting content, both does both options.
+                newline: '<p></p>', // Summernote's default is to use '<p><br></p>'
+                notStyle: 'position:absolute;top:0;left:0;right:0', // Position of Notification
+                icon: '<i class="note-icon">[Your Button]</i>',
+                keepHtml: false, // Remove all Html formats
+                keepClasses: false, // Remove Classes
+                badTags: ['style', 'script', 'applet', 'embed', 'noframes', 'font', 'noscript', 'html'], 
+                badAttributes: ['style', 'start','p'] // Remove attributes from remaining tags
+              }
             });
         });
         @include('connexion::shared.filemanager-modal-script',['folder'=>'books','width'=>250,'height'=>250])
--- src/Resources/views/books/edit.blade.php
+++ src/Resources/views/books/edit.blade.php
@@ -34,6 +34,7 @@
 @section('js')
 <script src="{{ asset('public/vendor/bishopm/js/selectize.min.js') }}" type="text/javascript"></script>
 <script src="{{ asset('public/vendor/bishopm/summernote/summernote.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('public/vendor/bishopm/summernote/summernote-cleaner.js') }}" type="text/javascript"></script>
 <script src="{{ asset('public/vendor/bishopm/js/croppie.js') }}" type="text/javascript"></script>
 <script>
     $.ajaxSetup({
@@ -47,18 +48,29 @@
             $("#slug").val(slug);
         });
         $('#description').summernote({ 
-          height: 250,
-          toolbar: [
-            ['style', ['bold', 'italic', 'underline', 'clear']],
-            ['font', ['superscript', 'subscript']],
-            ['fontsize', ['fontsize']],
-            ['color', ['color']],
-            ['table', ['table']],
-            ['link', ['linkDialogShow', 'unlink']],
-            ['view', ['fullscreen', 'codeview']],
-            ['para', ['ul', 'ol', 'paragraph']]
-          ]
-        });
+              height: 250,
+              toolbar: [
+                ['style', ['bold', 'italic', 'underline', 'clear']],
+                ['font', ['superscript', 'subscript']],
+                ['fontsize', ['fontsize']],
+                ['color', ['color']],
+                ['table', ['table']],
+                ['link', ['linkDialogShow', 'unlink']],
+                ['view', ['fullscreen', 'codeview']],
+                ['para', ['ul', 'ol', 'paragraph']]
+              ],
+              cleaner:{
+                notTime: 2400, // Time to display Notifications.
+                action: 'paste', // both|button|paste 'button' only cleans via toolbar button, 'paste' only clean when pasting content, both does both options.
+                newline: '<p></p>', // Summernote's default is to use '<p><br></p>'
+                notStyle: 'position:absolute;top:0;left:0;right:0', // Position of Notification
+                icon: '<i class="note-icon">[Your Button]</i>',
+                keepHtml: false, // Remove all Html formats
+                keepClasses: false, // Remove Classes
+                badTags: ['style', 'script', 'applet', 'embed', 'noframes', 'font', 'noscript', 'html'], 
+                badAttributes: ['style', 'start','p'] // Remove attributes from remaining tags
+              }
+            });
         $('.input-tags').selectize({
           plugins: ['remove_button'],
           openOnFocus: 0,
