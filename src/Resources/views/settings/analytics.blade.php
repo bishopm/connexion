4602cd11d1bffc11f68c984a665a17842a15ef5b
Michael Bishop <michael@umc.org.za>
2017-05-10 22:05:01 +0200

    Logging users

--- src/Resources/views/settings/analytics.blade.php
+++ src/Resources/views/settings/analytics.blade.php
@@ -8,10 +8,42 @@
                     <h2>Google Analytics</h2>
                 </div>
                 <div class="box-body">
-                    {{$analytics}}
+                    <table id="indexTable" class="table table-striped table-hover table-condensed table-responsive" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>Page</th>
+                                <th>Views</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                            <tr>
+                                <th>Page</th>
+                                <th>Views</th>
+                            </tr>
+                        </tfoot>
+                        <tbody>
+                            @forelse ($analytics as $analytic)
+                                <tr>
+                                    <td>{{$analytic->url}}</td>
+                                    <td>{{$analytic->pageViews}}</td>
+                                </tr>
+                            @empty
+                                <tr><td>No data has been added yet</td></tr>
+                            @endforelse
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
-    {!! Form::close() !!}
 @stop
+
+@section('js')
+<script language="javascript">
+$(document).ready(function() {
+        $('#indexTable').DataTable( {
+            "order": [[ 0, "desc" ]]
+        } );
+    } );
+</script>
+@endsection
