2790e592cd77829feeb40d885469827fc66ce2d1
Michael Bishop <michael@umc.org.za>
2017-05-12 12:02:46 +0200

    Allow leader only email

--- src/Resources/views/messages/results.blade.php
+++ src/Resources/views/messages/results.blade.php
@@ -5,8 +5,10 @@
 <div class="container-fluid">
   <table class="table">
     <tr><td colspan="2"><b>Messages sent to</b></td></tr>
-    @foreach ($recipients as $indiv)
-      <tr><td>{{$indiv['name']}}</td><td>{{$indiv['email']}}</td><td>{{$indiv['cellphone']}}</td></tr>
+    @foreach ($recipients as $household)
+    	@foreach ($household as $indiv)
+	      	<tr><td>{{$indiv['name']}}</td><td>{{$indiv['email']}}</td><td>{{$indiv['cellphone']}}</td></tr>
+	    @endforeach
     @endforeach
   </table>
 </div>
