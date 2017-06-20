d2cb6359934526c177afcedc95c87d0cd9dcfeea
Michael Bishop <michael@umc.org.za>
2017-06-20 10:22:35 +0200

    Auth testing

--- src/Http/Controllers/Auth/ApiAuthController.php
+++ src/Http/Controllers/Auth/ApiAuthController.php
@@ -14,7 +14,7 @@ class ApiAuthController extends Controller
     {
         // grab credentials from the request
         $credentials = $request->only('name', 'password');
-        Log::info('API login attempt: ' . $credentials);
+        Log::info('API login attempt: ' . json_encode($credentials));
         try {
             // attempt to verify the credentials and create a token for the user
             if (! $token = JWTAuth::attempt($credentials)) {
