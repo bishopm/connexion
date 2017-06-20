96b81d9e3cb0bd29f3e99c6ce280e39e7d98312b
Michael Bishop <michael@umc.org.za>
2017-06-20 10:10:07 +0200

    Log API login

--- src/Http/Controllers/Auth/ApiAuthController.php
+++ src/Http/Controllers/Auth/ApiAuthController.php
@@ -5,6 +5,7 @@ namespace Bishopm\Connexion\Http\Controllers\Auth;
 use JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Log;
 
 class ApiAuthController extends Controller
 {
@@ -12,7 +13,7 @@ class ApiAuthController extends Controller
     {
         // grab credentials from the request
         $credentials = $request->only('name', 'password');
-
+        Log::info('API login attempt: ' . $credentials);
         try {
             // attempt to verify the credentials and create a token for the user
             if (! $token = JWTAuth::attempt($credentials)) {
