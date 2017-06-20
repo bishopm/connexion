0ead9861a0d676a5564a6dcc6dd049edfa137954
Michael Bishop <michael@umc.org.za>
2017-06-20 10:16:36 +0200

    Add request to API auth

--- src/Http/Controllers/Auth/ApiAuthController.php
+++ src/Http/Controllers/Auth/ApiAuthController.php
@@ -5,6 +5,7 @@ namespace Bishopm\Connexion\Http\Controllers\Auth;
 use JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 class ApiAuthController extends Controller
