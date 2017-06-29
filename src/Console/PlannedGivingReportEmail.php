09f9ac5250cc91708a99e52b1c83b58f666fdc46
Michael Bishop <michael@umc.org.za>
2017-06-29 20:59:37 +0200

    Giving emails

--- src/Console/PlannedGivingReportEmail.php
+++ src/Console/PlannedGivingReportEmail.php
@@ -3,9 +3,9 @@
 namespace Bishopm\Connexion\Console;
 
 use Illuminate\Console\Command;
-use Bishopm\Connexion\Models\Book, Bishopm\Connexion\Models\Transaction, Bishopm\Connexion\Models\Supplier, Bishopm\Connexion\Models\Group;
+use Bishopm\Connexion\Models\Book, Bishopm\Connexion\Models\Payment, Bishopm\Connexion\Models\Individual;
 use Bishopm\Connexion\Models\Setting, DB;
-use Bishopm\Connexion\Mail\MonthlyBookshopMail;
+use Bishopm\Connexion\Mail\GivingMail;
 use Illuminate\Support\Facades\Mail;
 
 class PlannedGivingReportEmail extends Command
@@ -31,30 +31,81 @@ class PlannedGivingReportEmail extends Command
      */
     public function handle()
     {
-        $today="2017-06-29";
-        $thisyr=date('Y');
-        $lagtime=intval(Setting::where('setting_key','giving_lagtime')->first()->setting_value)
-        $reportnums=intval(Setting::where('setting_key','giving_reports')->first()->setting_value)
-        $period=12 / $reportnums ;
-        if ($reportnums==4){
-            
+        $today="2017-07-10";
+        $lagtime=intval(Setting::where('setting_key','giving_lagtime')->first()->setting_value);
+        echo "You have a lag setting of " . $lagtime . " days\n";
+        $effdate=strtotime($today)-$lagtime*86400;
+        $repyr=date('Y',$effdate);
+        echo "Your report year is " . $repyr . "\n";
+        $reportnums=intval(Setting::where('setting_key','giving_reports')->first()->setting_value);
+        echo "Your system will deliver " . $reportnums . " reports per year\n";
+        switch ($reportnums){
+            case 1:
+                $reportdates=array($repyr . "-12-31");
+                break;
+            case 2:
+                $reportdates=array($repyr . "-06-30",$repyr . "-12-31");
+                break;
+            case 3:
+                $reportdates=array($repyr . "-04-30",$repyr . "-08-31",$repyr . "-12-31");
+                break;
+            case 4:
+                $reportdates=array($repyr . "-03-31",$repyr . "-06-30",$repyr . "-09-30",$repyr . "-12-31");
+                break;
+            case 6:
+                $reportdates=array($repyr . "-02-28",$repyr . "-04-30",$repyr . "-06-30",$repyr . "-08-31",$repyr . "-10-31",$repyr . "-12-31");
+                break;
+            case 12:
+                $reportdates=array($repyr . "-01-31",$repyr . "-02-28",$repyr . "-03-31",$repyr . "-04-30",$repyr . "-05-31",$repyr . "-06-30",$repyr . "-07-31",$repyr . "-08-31",$repyr . "-09-30",$repyr . "-10-31",$repyr . "-11-30",$repyr . "-12-31");
+                break;
         }
-        $endoflastmonth=date('Y-m-t',strtotime("-1 month"));
-        $startofperiod=date('Y-m-01',strtotime("-" . $period . " month"));
-        dd($startofperiod, $endoflastmonth);
-        // Send to bookshop group
-        $setting=Setting::where('setting_key','bookshop')->first()->setting_value;
-        /*if ($setting){
-            $churchemail=Setting::where('setting_key','church_email')->first()->setting_value;
-            $group=Group::with('individuals')->where('groupname',$setting)->first();
-            if ($group){
-                foreach ($group->individuals as $recip){
-                    $data['recipient']=$recip->firstname;
-                    $data['subject']="Bookshop sales and stock report: " . date("M Y", strtotime("first day of previous month"));
-                    $data['sender']=$churchemail;
-                    Mail::to($recip->email)->send(new MonthlyBookshopMail($data));
+        if (in_array(date("Y-m-d",$effdate),$reportdates)){
+            $period=12/$reportnums;
+            $endofperiod=date('Y-m-t',$effdate);
+            $startmonth=intval(date('m',$effdate))-$period+1;
+            if ($startmonth<1){
+                $startmonth=$startmonth+12;
+            }
+            if ($startmonth<10){
+                $sm="0" . strval($startmonth);
+            } else {
+                $sm=strval($startmnonth);
+            }
+            $startofperiod=$repyr . "-" . $sm . "-01";
+            echo "Calculating totals for the period: " . $startofperiod . " to " . $endofperiod . "\n";
+            $givers=Individual::where('giving','>',1)->where('surname','Bishop')->get();
+            $data=array();
+            foreach ($givers as $giver){
+                $data[$giver->giving]['email'][]=$giver->email;
+                if (count($data[$giver->giving]['email'])==1){
+                    $data[$giver->giving]['period']=$startofperiod . " to " . $endofperiod;
+                    $data[$giver->giving]['sender']=Setting::where('setting_key','church_email')->first()->setting_value;
+                    $data[$giver->giving]['pg']=$giver->giving;
+                    $data[$giver->giving]['pgyr']=$repyr;                    
+                    $data[$giver->giving]['church']=Setting::where('setting_key','site_abbreviation')->first()->setting_value;
+                    if ($period==1){
+                        $data[$giver->giving]['scope']="month";
+                    } else {
+                        $data[$giver->giving]['scope']=$period . " months";
+                    }
+                    $data[$giver->giving]['subject']="Planned giving feedback: " . $startofperiod . " to " . $endofperiod;
+                    $currentpayments=Payment::where('pgnumber',$giver->giving)->where('paymentdate','>=',$startofperiod)->where('paymentdate','<=',$endofperiod)->orderBy('paymentdate','DESC')->get();
+                    foreach ($currentpayments as $cp){
+                        $data[$giver->giving]['current'][]=$cp;
+                    }
+                    $historicpayments=Payment::where('pgnumber',$giver->giving)->where('paymentdate','<',$startofperiod)->where('paymentdate','>=',$repyr . '-01-01')->orderBy('paymentdate','DESC')->get();
+                    foreach ($historicpayments as $hp){
+                        $data[$giver->giving]['historic'][]=$hp;
+                    }
                 }
             }
-        }*/
+            foreach ($data as $key=>$pg){
+                foreach ($pg['email'] as $indiv){
+                    Mail::to($indiv)->send(new GivingMail($pg));
+                }
+            }
+        } else {
+            echo "Today is not a report date (" . date("Y-m-d",$effdate) . ")\n";
+        }
     }
 }
---
+++ src/Resources/views/emails/givingreport.blade.php
@@ -0,0 +1,42 @@
+@component('mail::message')
+# {{$data['subject']}}
+
+Dear Planned Giver **{{$data['pg']}}**
+
+Thank you so much for making ministry possible at {{$data['church']}} by participating in our Planned Giving programme.
+
+Please find below for your records a breakdown of your planned giving for the last {{$data['scope']}}. This mail is to help you to track your giving and ensure that our record-keeping is accurate. 
+
+This email is system-generated to preserve anonymity.
+
+Please feel free to contact the church office if you have any questions or concerns.
+
+# Last {{$data['scope']}}
+@if (count($data['current']))
+@component('mail::table')
+| Date received | Amount | 
+| -----|-----------:| 
+@foreach ($data['current'] as $payment)
+|{{$payment->paymentdate}}|{{number_format($payment->amount,2)}}|
+@endforeach
+@endcomponent
+@else
+No payments received during this period
+@endif
+
+@if (count($data['historic']))
+# Other {{$data['pgyr']}} payments (prior to the last {{$data['scope']}})
+
+@component('mail::table')
+| Date received | Amount | 
+| -----|-----------:| 
+@foreach ($data['historic'] as $payment)
+|{{$payment->paymentdate}}|{{number_format($payment->amount,2)}}|
+@endforeach
+@endcomponent
+@endif
+
+May God bless and encourage you as you continue to serve Him.
+
+Thank you!
+@endcomponent
