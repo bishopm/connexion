5982e9745e60ed871c3132a2f7ef6a1674ce15ff
Michael Bishop <michael@umc.org.za>
2017-09-14 21:45:11 +0200

    Tweak for Circuit repository

--- src/Repositories/CircuitsRepository.php
+++ src/Repositories/CircuitsRepository.php
@@ -5,5 +5,10 @@ use Bishopm\Connexion\Models\Setting;
 
 class CircuitsRepository extends McsaBaseRepository
 {
-
+    public function all()
+    {
+        $url = $this->api_url . $this->circuit . '/' . $this->model . '?token=' . $this->token;
+        $res = $this->client->request('GET', $url);
+        return $res->getBody()->getContents();
+    }
 }
