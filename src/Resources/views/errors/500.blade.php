4a4ada7731cbf21fdfc360bb5799387493a7bbc4
Michael Bishop <michael@umc.org.za>
2017-10-03 17:29:00 +0200

    Custom error message

---
+++ src/Resources/views/errors/500.blade.php
@@ -0,0 +1,9 @@
+@extends('connexion::templates.frontend')
+
+@section('content')  
+    <div class="container top30">
+    	<h1>Oops - something has gone wrong!</h1>
+    	<p>Sorry for the inconvenience!</p>
+	<p>If it makes you feel any better, Michael's phone has just pinged and we'll try and sort out the problem asap :)</p>
+    </div>
+@stop
