973613704d68fd5bffa6f5cd36870f1819fe4411
Michael Bishop <michael@goodworx.co.za>
2017-11-14 11:54:23 +0200

    Include additional info in Messages API

--- src/Repositories/MessagesRepository.php
+++ src/Repositories/MessagesRepository.php
@@ -1,12 +1,19 @@
 <?php namespace Bishopm\Connexion\Repositories;
 
 use Bishopm\Connexion\Repositories\EloquentBaseRepository;
+use Carbon\Carbon;
 
 class MessagesRepository extends EloquentBaseRepository
 {
     public function userMessages($id)
     {
-    	return $this->model->where('receiver_id','=',$id)->orderBy('created_at')->get();
+        $messages = $this->model->where('receiver_id','=',$id)->orderBy('created_at')->get();
+        foreach ($messages as $message){
+            $message->sender = $message->user->individual->firstname . " " . $message->user->individual->surname;
+            $message->senderpic = url('/') . "/storage/individuals/" . $message->user->id . "/" . $message->user->individual->image;
+            $message->ago = Carbon::parse($message->created_at)->diffForHumans();
+        }
+        return $messages;
     }
 
 }
