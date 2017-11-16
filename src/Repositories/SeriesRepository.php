3eb5496fdcfe205267c223b67cd43feb2fca3952
Michael Bishop <michael@umc.org.za>
2017-11-16 15:58:33 +0200

    Another attempt at series info

--- src/Repositories/SeriesRepository.php
+++ src/Repositories/SeriesRepository.php
@@ -1,12 +1,20 @@
 <?php namespace Bishopm\Connexion\Repositories;
 
 use Bishopm\Connexion\Repositories\EloquentBaseRepository;
+use Carbon\Carbon;
 
 class SeriesRepository extends EloquentBaseRepository
 {
     public function findwithsermons($id)
     {
-        return $this->model->with('sermons.comments','sermons.individual')->where('id',$id)->first();
+        $series = $this->model->with('sermons.comments.users.individuals')->where('id',$id)->first();
+        foreach ($series->sermons as $sermon){
+            foreach ($sermon->comments as $comment){
+                $author=$comment->user->individual->firstname . " " . $comment->user->individual->surname;
+                $comment->image = "http://umc.org.za/storage/individuals/" . $comment->user->individual_id . "/" . $comment->user->individual->image;
+            }
+        }
+        return $series;
     }
 
 
