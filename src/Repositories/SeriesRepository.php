1249e9eb7cce4f15a2625165e2ec41acbb94f346
Michael Bishop <michael@umc.org.za>
2017-11-16 15:41:43 +0200

    Adjust series API

--- src/Repositories/SeriesRepository.php
+++ src/Repositories/SeriesRepository.php
@@ -6,15 +6,7 @@ class SeriesRepository extends EloquentBaseRepository
 {
     public function findwithsermons($id)
     {
-        $series = $this->model->with('sermons.comments','sermons.individual')->where('id',$id)->first();
-        foreach ($series->sermons as $sermon){
-            foreach ($sermon->comments as $comment){
-                $author=$this->user->find($comment->commented_id);
-                $comment->author = $author->individual->firstname . " " . $author->individual->surname;
-                $comment->image = "http://umc.org.za/public/storage/individuals/" . $author->individual_id . "/" . $author->individual->image;
-            }
-        }
-        return $series;
+        return $this->model->with('sermons.comments','sermons.individual')->where('id',$id)->first();
     }
 
 
