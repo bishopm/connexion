0d667c0efddb304f97be168829bfda0d8bc80222
Michael Bishop <michael@umc.org.za>
2017-11-20 14:50:21 +0200

    Typo

--- src/Repositories/GroupsRepository.php
+++ src/Repositories/GroupsRepository.php
@@ -42,7 +42,7 @@ class GroupsRepository extends EloquentBaseRepository
 
 	public function find($id)
     {
-        return $this->model->with('individuals','individuals.users')->find($id);
+        return $this->model->with('individuals','individuals.user')->find($id);
     }
 
     public function findByName($name)
