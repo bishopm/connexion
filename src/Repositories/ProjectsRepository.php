fd81461b17ff42e0ffdef0755a9097d6366bc0c2
Michael Bishop <michael@umc.org.za>
2017-11-23 12:42:22 +0200

    Fix projects API

--- src/Repositories/ProjectsRepository.php
+++ src/Repositories/ProjectsRepository.php
@@ -21,7 +21,7 @@ class ProjectsRepository extends EloquentBaseRepository
         return $this->model->where('individual_id',$indiv)->OrderBy('description')->get();
     }
 
-    public function findForApi($indiv){
-        return $this->model->with('actions','individual')->where('individual_id',$indiv)->first();
+    public function findForApi($id){
+        return $this->model->with('actions','individual')->where('id',$id)->first();
     }
 }
