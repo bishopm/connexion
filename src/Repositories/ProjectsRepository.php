d7cd1de62e3826a081154fd8c00a2df8bae83e34
Michael Bishop <michael@umc.org.za>
2017-11-23 12:25:22 +0200

    Typos

--- src/Repositories/ProjectsRepository.php
+++ src/Repositories/ProjectsRepository.php
@@ -18,6 +18,10 @@ class ProjectsRepository extends EloquentBaseRepository
     }
 
     public function allForApi($indiv){
-        return $this->model->with('actions','individual')->where('individual_id',$indiv)->OrderBy('description')->get();
+        return $this->model->where('individual_id',$indiv)->OrderBy('description')->get();
+    }
+
+    public function findForApi($indiv){
+        return $this->model->with('actions','individual')->where('individual_id',$indiv)->first();
     }
 }
