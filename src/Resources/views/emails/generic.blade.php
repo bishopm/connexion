27b4e2f4b3bfe1837d0b0151aa6f1325b793e4ad
Michael Bishop <michael@goodworx.co.za>
2018-04-13 13:18:18 +0200

    Fix giving emails

--- src/Resources/views/emails/generic.blade.php
+++ src/Resources/views/emails/generic.blade.php
@@ -1,26 +1,5 @@
-@component('mail::layout')
-@slot('header')
-@component('mail::header', ['url' => config('app.url')])
-{{$emaildata->header}}
-@endcomponent
-@endslot
+@component('mail::message')
 # {{$emaildata->subject}}
 
-{!! $emaildata->emailmessage !!}
-
-@slot('footer')
-@component('mail::footer')
-<i>{{$emaildata->footer}}</i><br>
-{{$emaildata->address}}<br><br>
-@if ($emaildata->facebook)
-    <a title="Like us on Facebook" target="_blank" href="{{$emaildata->facebook}}"><img alt="Like us on Facebook" style="margin-right:5px; margin-left:5px" width="25px" src="{{asset('/vendor/bishopm/images/facebook.png')}}"></a>
-@endif
-@if ($emaildata->twitter)
-    <a title="Follow us on Twitter" target="_blank" href="{{$emaildata->twitter}}"><img alt="Follow us on Twitter" style="margin-right:5px; margin-left:5px" width="25px" src="{{asset('/vendor/bishopm/images/twitter.png')}}"></a>
-@endif
-@if ($emaildata->youtube)
-    <a title="Youtube profile page" target="_blank" href="{{$emaildata->youtube}}"><img alt="Youtube profile page" style="margin-right:5px; margin-left:5px" width="25px" src="{{asset('/vendor/bishopm/images/youtube.png')}}"></a>
-@endif
-@endcomponent
-@endslot
+{!!$emaildata->emailmessage!!}
 @endcomponent
