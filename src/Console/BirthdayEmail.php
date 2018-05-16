84ab709ce5526e5de6ff9a355f17458c5fffc2b2
Michael Bishop <michael@goodworx.co.za>
2018-05-16 12:34:34 +0200

    Remove deleted members from birthday list

--- src/Console/BirthdayEmail.php
+++ src/Console/BirthdayEmail.php
@@ -3,8 +3,12 @@
 namespace Bishopm\Connexion\Console;
 
 use Illuminate\Console\Command;
-use Bishopm\Connexion\Models\Individual, Bishopm\Connexion\Models\Household, Bishopm\Connexion\Models\Specialday;
-use Bishopm\Connexion\Models\Setting, Bishopm\Connexion\Models\Group, DB;
+use Bishopm\Connexion\Models\Individual;
+use Bishopm\Connexion\Models\Household;
+use Bishopm\Connexion\Models\Specialday;
+use Bishopm\Connexion\Models\Setting;
+use Bishopm\Connexion\Models\Group;
+use DB;
 use Bishopm\Connexion\Mail\BirthdayMail;
 use Illuminate\Support\Facades\Mail;
 
@@ -26,11 +30,11 @@ class BirthdayEmail extends Command
 
     public function gethcell($id)
     {
-      $indiv=Individual::find($id);
-      if ($indiv) {
-          return $indiv->firstname . " (" . $indiv->cellphone . ")";
-      }
-      return "Invalid individual";
+        $indiv=Individual::find($id);
+        if ($indiv) {
+            return $indiv->firstname . " (" . $indiv->cellphone . ")";
+        }
+        return "Invalid individual";
     }
 
     /**
@@ -41,50 +45,51 @@ class BirthdayEmail extends Command
     public function handle()
     {
         $thisyr=date("Y");
-        $mon=strval(date('m-d',strtotime("next Monday")));
-        $tue=strval(date('m-d',strtotime("next Monday")+86400));
-        $wed=strval(date('m-d',strtotime("next Monday")+172800));
-        $thu=strval(date('m-d',strtotime("next Monday")+259200));
-        $fri=strval(date('m-d',strtotime("next Monday")+345600));
-        $sat=strval(date('m-d',strtotime("next Monday")+432000));
-        $sun=strval(date('m-d',strtotime("next Monday")+518400));
+        $mon=strval(date('m-d', strtotime("next Monday")));
+        $tue=strval(date('m-d', strtotime("next Monday")+86400));
+        $wed=strval(date('m-d', strtotime("next Monday")+172800));
+        $thu=strval(date('m-d', strtotime("next Monday")+259200));
+        $fri=strval(date('m-d', strtotime("next Monday")+345600));
+        $sat=strval(date('m-d', strtotime("next Monday")+432000));
+        $sun=strval(date('m-d', strtotime("next Monday")+518400));
         $msg="Birthdays for the week: (starting " . $thisyr . "-" . $mon . ")<br><br>";
         $days=array($mon,$tue,$wed,$thu,$fri,$sat,$sun);
         //DB::enableQueryLog();
-        $birthdays=Individual::join('households', 'household_id', '=', 'households.id')->select('individuals.id','homephone','householdcell','cellphone','firstname','surname',DB::raw('substr(birthdate, 6, 5) as bd'))->wherein(DB::raw('substr(birthdate, 6, 5)'),$days)->orderBy(DB::raw('substr(birthdate, 6, 5)'))->get();
+        $birthdays=Individual::join('households', 'household_id', '=', 'households.id')->select('individuals.id', 'homephone', 'householdcell', 'cellphone', 'firstname', 'surname', DB::raw('substr(birthdate, 6, 5) as bd'))->wherein(DB::raw('substr(birthdate, 6, 5)'), $days)->whereNull('individuals.deleted_at')->orderBy(DB::raw('substr(birthdate, 6, 5)'))->get();
+        dd($birthdays);
         //Log::debug(DB::getQueryLog());
-        foreach($birthdays as $bday){
-            $msg=$msg . "*" . date("D d M",strtotime($thisyr . "-" . $bday->bd)) . "* **" . $bday->firstname . " " . $bday->surname . ":**";
-            if ($bday->cellphone){
+        foreach ($birthdays as $bday) {
+            $msg=$msg . "*" . date("D d M", strtotime($thisyr . "-" . $bday->bd)) . "* **" . $bday->firstname . " " . $bday->surname . ":**";
+            if ($bday->cellphone) {
                 $msg=$msg . " Cellphone: " . $bday->cellphone;
             }
-            if ($bday->homephone){
+            if ($bday->homephone) {
                 $msg=$msg . " Homephone: " . $bday->homephone;
             }
-            if (($bday->householdcell) and ($bday->householdcell<>$bday->id)){
+            if (($bday->householdcell) and ($bday->householdcell<>$bday->id)) {
                 $msg=$msg . " Household cellphone: " . self::gethcell($bday->householdcell);
             }
             $msg=$msg . "<br>";
             //Log::notice($bday->surname . ", " . $bday->firstname);
         }
-        $anniversaries=Specialday::join('households', 'household_id', '=', 'households.id')->select('homephone','householdcell','addressee','household_id','anniversarytype','details',DB::raw('substr(anniversarydate, 6, 5) as ad'))->wherein(DB::raw('substr(anniversarydate, 6, 5)'),$days)->orderBy(DB::raw('substr(anniversarydate, 6, 5)'))->get();
+        $anniversaries=Specialday::join('households', 'household_id', '=', 'households.id')->select('homephone', 'householdcell', 'addressee', 'household_id', 'anniversarytype', 'details', DB::raw('substr(anniversarydate, 6, 5) as ad'))->wherein(DB::raw('substr(anniversarydate, 6, 5)'), $days)->orderBy(DB::raw('substr(anniversarydate, 6, 5)'))->get();
         $msg = $msg . "<br>" . "Anniversaries" . "<br><br>";
-        foreach ($anniversaries as $ann){
-            $msg=$msg . date("D d M",strtotime($thisyr . "-" . $ann->ad)) . " (" . $ann->addressee . ". " . ucfirst($ann->anntype) . ": " . $ann->details. ")";
-            if ($ann->homephone){
+        foreach ($anniversaries as $ann) {
+            $msg=$msg . date("D d M", strtotime($thisyr . "-" . $ann->ad)) . " (" . $ann->addressee . ". " . ucfirst($ann->anntype) . ": " . $ann->details. ")";
+            if ($ann->homephone) {
                 $msg=$msg . " Homephone: " . $ann->homephone;
             }
-            if ($ann->householdcell){
+            if ($ann->householdcell) {
                 $msg=$msg . " Household cellphone: " . self::gethcell($ann->householdcell);
             }
             $msg=$msg. "<br>";
         }
         // Send to birthday group
-        $setting=Setting::where('setting_key','birthday_group')->first()->setting_value;
-        $churchname=Setting::where('setting_key','site_name')->first()->setting_value;
-        $churchemail=Setting::where('setting_key','church_email')->first()->setting_value;
+        $setting=Setting::where('setting_key', 'birthday_group')->first()->setting_value;
+        $churchname=Setting::where('setting_key', 'site_name')->first()->setting_value;
+        $churchemail=Setting::where('setting_key', 'church_email')->first()->setting_value;
         $group=Group::with('individuals')->find($setting);
-        foreach ($group->individuals as $recip){
+        foreach ($group->individuals as $recip) {
             $data['recipient']=$recip->firstname;
             $data['subject']="Birthday email from " . $churchname;
             $data['sender']=$churchemail;
