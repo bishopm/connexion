62f08f324baa785bb29c47eec59229c5915c0dc7
Michael Bishop <michael@goodworx.co.za>
2019-01-30 12:36:41 +0200

    Remove old crons

--- src/Providers/ScheduleServiceProvider.php
+++ src/Providers/ScheduleServiceProvider.php
@@ -11,10 +11,8 @@ class ScheduleServiceProvider extends ServiceProvider
     {
         $this->app->booted(function () {
             $schedule = $this->app->make(Schedule::class);
-            $schedule->command('connexion:supplieremails')->monthlyOn(1,'07:15');
-            $schedule->command('connexion:bookshopemails')->monthlyOn(1,'07:30');
-            $schedule->command('connexion:givingemails')->dailyAt('07:45');
-            $schedule->command('connexion:birthdayemail')->weekly()->mondays()->at('08:00');            
+            $schedule->command('connexion:supplieremails')->monthlyOn(1, '07:15');
+            $schedule->command('connexion:bookshopemails')->monthlyOn(1, '07:30');
         });
     }
 
